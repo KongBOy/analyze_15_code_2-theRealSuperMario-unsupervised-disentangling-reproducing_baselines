c34bcfb1df79bef42ed512e373ac28cd139b8841
Sandro Braun <supermario94123@gmail.com>
2020-04-29 15:35:50 +0200

    refactors model_pt

--- src/torch/model_pt.py
+++ src/torch/model_pt.py
@@ -21,7 +21,6 @@ from dotmap import DotMap
 class Model(torch.nn.Module):
     def __init__(
         self,
-        tps_par,
         heat_dim,
         nFeat_1,
         nFeat_2,
@@ -39,17 +38,9 @@ class Model(torch.nn.Module):
         n_parts,
         adversarial,
         patch_size,
-        in_dim,
-        contrast_var,
-        brightness_var,
-        saturation_var,
-        hue_var,
-        p_flip,
-        l_2_scal,
-        l_2_threshold,
+        **kwargs
     ):
         super(Model, self).__init__()
-        self.tps_par = tps_par
         self.nFeat_1 = nFeat_1
         self.nFeat_2 = nFeat_2
         self.n_features = 64
@@ -72,56 +63,14 @@ class Model(torch.nn.Module):
         self.n_parts = n_parts
         self.adversarial = adversarial
         self.patch_size = patch_size
-        self.in_dim = in_dim
-        self.contrast_var = contrast_var
-        self.brightness_var = brightness_var
-        self.saturation_var = saturation_var
-        self.hue_var = hue_var
-        self.p_flip = p_flip
         if self.adversarial:
             self.discriminator = architectures_pt.Discriminator_Patch()
-        self.l_2_threshold = l_2_threshold
-        self.l_2_scal = l_2_scal
 
-    def forward(self, x):
-        image_orig = x
-        coord, vector = make_input_tps_param(self.tps_par)
-        t_images, t_mesh = ThinPlateSpline(
-            image_orig, coord, vector, self.in_dim, self.n_c
-        )
-        image_in, image_rec = prepare_pairs(
-            t_images,
-            self.reconstr_dim,
-            train=self.training,
-            static=self.static,
-            contrast_var=self.contrast_var,
-            brightness_var=self.brightness_var,
-            saturation_var=self.saturation_var,
-            hue_var=self.hue_var,
-            p_flip=self.p_flip,
-        )
-        transform_mesh = F.interpolate(t_mesh, (self.heat_dim, self.heat_dim))
-        volume_mesh = AbsDetJacobian(transform_mesh)
-
-        part_maps, raw_features = self.encoder(
-            image_in
-        )  # [2, 16, 64, 64] and [2, 64, 64, 64]
+    def forward(self, image_in, image_rec=None, static=False):
+        part_maps, raw_features = self.encoder(image_in)
 
         mu, L_inv = part_map_to_mu_L_inv(part_maps, self.L_inv_scal)
         features = get_features(raw_features, part_maps, True)
-        integrant = torch.squeeze(
-            torch.unsqueeze(part_maps, dim=-1) * torch.unsqueeze(volume_mesh, dim=-1)
-        )
-        integrant = integrant / torch.sum(integrant, dim=[2, 3], keepdims=True)
-        mu_t = torch.einsum("akij,alij->akl", integrant, transform_mesh)
-        transform_mesh_out_prod = torch.einsum(
-            "amij,anij->aijmn", transform_mesh, transform_mesh
-        )
-        mu_out_prod = torch.einsum("akm,akn->akmn", mu_t, mu_t)
-        stddev_t = (
-            torch.einsum("akij,aijmn->akmn", integrant, transform_mesh_out_prod)
-            - mu_out_prod
-        )  # [2, 16, 2, 2]
 
         encoding_same_id = feat_mu_to_enc(
             features,
@@ -134,7 +83,7 @@ class Model(torch.nn.Module):
             covariance=self.covariance,
             feat_shape=self.average_features_mode,
             heat_feat_normalize=self.heat_feat_normalize,
-            static=self.static,
+            static=static,
         )
         encoding_same_id = [
             tfpyth.th_2D_channels_last_to_first(e) for e in encoding_same_id
@@ -143,17 +92,10 @@ class Model(torch.nn.Module):
         reconstruct_same_id = self.img_decoder(encoding_same_id)
 
         outputs = {
-            "image_in": image_in,
-            "image_rec": image_rec,
-            "transform_mesh": transform_mesh,
-            "volume_mesh": volume_mesh,
             "part_maps": part_maps,
             "mu": mu,
             "L_inv": L_inv,
             "features": features,
-            "integrant": integrant,
-            "mu_t": mu_t,
-            "stddev_t": stddev_t,
             "reconstruct_same_id": reconstruct_same_id,
             "encoding_same_id": encoding_same_id,
         }
@@ -194,42 +136,51 @@ class Model(torch.nn.Module):
         return outputs
 
 
-def make_visualizations(out, model, heat_mask_l2):
+def make_visualizations(
+    out,
+    image_in,
+    image_rec,
+    heat_mask_l2,
+    l_2_threshold,
+    in_dim,
+    part_depths,
+    n_c,
+    n_parts,
+    bn,
+    adversarial,
+):
     visualizations = {}
 
-    visualizations["g_reconstr"] = out.image_rec
-    normal = utils.part_to_color_map(
-        out.encoding_same_id, model.part_depths, size=model.in_dim
-    )
+    visualizations["g_reconstr"] = image_rec
+    normal = utils.part_to_color_map(out.encoding_same_id, part_depths, size=in_dim)
     normal = normal / (1 + torch.sum(normal, dim=1, keepdim=True))
     vis_normal = torch.where(
         ops_pt.tile_nd(torch.sum(normal, dim=1, keepdim=True), [1, 3, 1, 1]) > 0.3,
         normal,
-        out.image_in,
+        image_in,
     )
     heat_mask_l2 = torch.nn.functional.interpolate(
-        ops_pt.tile_nd(heat_mask_l2, [1, 3, 1, 1]), size=(model.in_dim, model.in_dim),
+        ops_pt.tile_nd(heat_mask_l2, [1, 3, 1, 1]), size=(in_dim, in_dim),
     )
-    vis_normal = torch.where(
-        heat_mask_l2 > model.l_2_threshold, vis_normal, 0.3 * vis_normal
-    )
-    visualizations["gt_t_1"] = vis_normal[: model.bn, ...]
-    visualizations["gt_t_2"] = vis_normal[model.bn :, ...]
-    visualizations["part_maps_t_1"] = utils.batch_colour_map(
-        out.part_maps[: model.bn, ...]
-    )
-    visualizations["part_maps_t_2"] = utils.batch_colour_map(
-        out.part_maps[model.bn :, ...]
-    )
+    vis_normal = torch.where(heat_mask_l2 > l_2_threshold, vis_normal, 0.3 * vis_normal)
+    visualizations["gt_t_1"] = vis_normal[:bn, ...]
+    visualizations["gt_t_2"] = vis_normal[bn:, ...]
+    visualizations["part_maps_t_1"] = utils.batch_colour_map(out.part_maps[:bn, ...])
+    visualizations["part_maps_t_2"] = utils.batch_colour_map(out.part_maps[bn:, ...])
 
-    if model.adversarial:
-        f_dim = 2 * model.bn * model.n_parts
-        visualizations["patch_real"] = out.patches[:f_dim, : model.n_c, :, :]
+    if adversarial:
+        f_dim = 2 * bn * n_parts
+        visualizations["patch_real"] = out.patches[:f_dim, :n_c, :, :]
         visualizations["patch_fake"] = out.patches[
-            f_dim : f_dim + f_dim // 2, : model.n_c, :, :
+            f_dim : f_dim + f_dim // 2, :n_c, :, :
         ]
         visualizations["same_id_reconstruction"] = out.reconstruct_same_id
         visualizations["same_id_reconstruction"] = utils.summary_feat_and_parts(
-            out.encoding_list, model.part_depths, False
+            out.encoding_list, part_depths, False
         )
+
+    # scale from range [0, 1] to [-1, 1]
+    for k, v, in visualizations.items():
+        visualizations[k] = (v - 0.5) * 2
+
     return visualizations
--- test/test_model_pt.py
+++ test/test_model_pt.py
@@ -2,6 +2,7 @@ from src.tf.model import ModelArgs
 from supermariopy.ptutils import tps
 from src.torch import ops_pt, model_pt
 import torch
+import torch.nn.functional as F
 
 
 class Test_model:
@@ -12,7 +13,7 @@ class Test_model:
         C = 3
         args = ModelArgs(bn=1)
         image_batch = torch.zeros((N, C, H, W), dtype=torch.float32)
-        image_batch_tiled = ops_pt.tile_nd(image_batch, [2, 1, 1, 1])
+        image_batch_tiled = ops_pt.tile_nd(image_batch, [2, 1, 1, 1]).cuda()
         arg = ModelArgs(bn=1)
 
         tps_params = tps.no_transformation_parameters(2 * N)
@@ -22,7 +23,6 @@ class Test_model:
         tps_param_dic = DotMap(tps_param_dic)
 
         model = model_pt.Model(
-            tps_param_dic,
             heat_dim=args.heat_dim,
             nFeat_1=args.nFeat1,
             nFeat_2=args.nFeat2,
@@ -40,12 +40,114 @@ class Test_model:
             n_parts=args.n_parts,
             adversarial=False,
             patch_size=args.patch_size,
-            in_dim=args.in_dim,
+        )
+
+        coord, vector = tps.make_input_tps_param(tps_param_dic)
+        t_images, t_mesh = tps.ThinPlateSpline(
+            image_batch_tiled, coord, vector, args.in_dim, args.n_c
+        )
+        image_in, image_rec = ops_pt.prepare_pairs(
+            t_images,
+            model.reconstr_dim,
+            train=True,
+            static=False,
             contrast_var=args.contrast_var,
             brightness_var=args.brightness_var,
             saturation_var=args.saturation_var,
             hue_var=args.hue_var,
             p_flip=args.p_flip,
         )
+        model = model.cuda()
+        out = model(image_in)
+        mu = out.mu
+        part_maps = out.part_maps
+        transform_mesh = F.interpolate(t_mesh, (args.heat_dim, args.heat_dim))
+        volume_mesh = ops_pt.AbsDetJacobian(transform_mesh)
 
-        out = model(image_batch_tiled)
+        integrant = torch.squeeze(
+            torch.unsqueeze(part_maps, dim=-1) * torch.unsqueeze(volume_mesh, dim=-1)
+        )
+        integrant = integrant / torch.sum(integrant, dim=[2, 3], keepdims=True)
+        mu_t = torch.einsum("akij,alij->akl", integrant, transform_mesh)
+        transform_mesh_out_prod = torch.einsum(
+            "amij,anij->aijmn", transform_mesh, transform_mesh
+        )
+        mu_out_prod = torch.einsum("akm,akn->akmn", mu_t, mu_t)
+        stddev_t = (
+            torch.einsum("akij,aijmn->akmn", integrant, transform_mesh_out_prod)
+            - mu_out_prod
+        )  # [2, 16, 2, 2]
+
+    def test_adversarial(self):
+        N = 1
+        H = 128
+        W = 128
+        C = 3
+        args = ModelArgs(bn=1, adversarial=True)
+        image_batch = torch.zeros((N, C, H, W), dtype=torch.float32)
+        image_batch_tiled = ops_pt.tile_nd(image_batch, [2, 1, 1, 1]).cuda()
+
+        tps_params = tps.no_transformation_parameters(2 * N)
+        tps_param_dic = tps.tps_parameters(**tps_params)
+        from dotmap import DotMap
+
+        tps_param_dic = DotMap(tps_param_dic)
+
+        model = model_pt.Model(
+            heat_dim=args.heat_dim,
+            nFeat_1=args.nFeat1,
+            nFeat_2=args.nFeat2,
+            L_inv_scal=args.L_inv_scal,
+            rec_stages=args.rec_stages,
+            part_depths=args.part_depths,
+            feat_slices=args.feat_slices,
+            covariance=args.covariance,
+            average_features_mode=args.average_features_mode,
+            heat_feat_normalize=args.heat_feat_normalize,
+            static=args.static,
+            reconstr_dim=args.reconstr_dim,
+            n_c=args.n_c,
+            bn=args.bn,
+            n_parts=args.n_parts,
+            adversarial=args.adversarial,
+            patch_size=args.patch_size,
+            in_dim=args.in_dim,
+            l_2_scal=args.l_2_scal,
+            l_2_threshold=args.l_2_threshold,
+        )
+
+        coord, vector = tps.make_input_tps_param(tps_param_dic)
+        t_images, t_mesh = tps.ThinPlateSpline(
+            image_batch_tiled, coord, vector, model.in_dim, model.n_c
+        )
+        image_in, image_rec = ops_pt.prepare_pairs(
+            t_images,
+            model.reconstr_dim,
+            train=True,
+            static=False,
+            contrast_var=model.contrast_var,
+            brightness_var=model.brightness_var,
+            saturation_var=model.saturation_var,
+            hue_var=model.hue_var,
+            p_flip=model.p_flip,
+        )
+        model = model.cuda()
+        out = model(image_in, image_rec)
+        mu = out.mu
+        part_maps = out.part_maps
+        transform_mesh = F.interpolate(t_mesh, (model.heat_dim, model.heat_dim))
+        volume_mesh = ops_pt.AbsDetJacobian(transform_mesh)
+
+        integrant = torch.squeeze(
+            torch.unsqueeze(part_maps, dim=-1) * torch.unsqueeze(volume_mesh, dim=-1)
+        )
+        integrant = integrant / torch.sum(integrant, dim=[2, 3], keepdims=True)
+        mu_t = torch.einsum("akij,alij->akl", integrant, transform_mesh)
+        transform_mesh_out_prod = torch.einsum(
+            "amij,anij->aijmn", transform_mesh, transform_mesh
+        )
+        mu_out_prod = torch.einsum("akm,akn->akmn", mu_t, mu_t)
+        stddev_t = (
+            torch.einsum("akij,aijmn->akmn", integrant, transform_mesh_out_prod)
+            - mu_out_prod
+        )  # [2, 16, 2, 2]
